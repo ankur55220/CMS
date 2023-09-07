72e224636769467317920e6d872979cdc8b1b962
Ankur <ankur55420@gmail.com>
2023-09-07 23:49:43 +0530

    last try for build today

--- front/src/vite.config.js
+++ front/src/vite.config.js
@@ -6,7 +6,19 @@ export default defineConfig({
 
   base: '',
   root: 'src',
-  build: { outDir: '../dist', emptyOutDir: true },
+  build: { outDir: '../dist', 
+          emptyOutDir: true ,
+          rollupOptions: {
+            external: ["react", "react-router", "react-router-dom", "react-redux"],
+            output: {
+              globals: {
+                react: "React",
+              },
+            },
+          },  
+        
+        
+        },
   plugins:[react()],
   
 })
