dc81270e29f3e8b4de9143e7623f878c243bd2dc
Ankur verma <38664111+ankur55220@users.noreply.github.com>
2023-09-07 22:46:22 +0530

    Update vite.config.js

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+      base: '',
+      root: 'src',
+      build: { outDir: '../dist', emptyOutDir: true }
   plugins: [react()],
 })
